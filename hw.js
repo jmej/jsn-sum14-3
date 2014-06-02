3a6f0489abecda1c3e26bbefbfc046def05b7d4e
Jesse Mejia <jmejia@mercycorps.org>
2014-06-02 15:18:10 -0700

    finished pt 1b of hw3

--- hw.js
+++ hw.js
@@ -21,7 +21,8 @@ function makeCard(id) {
 	card.name = makeCard.name;
 	card.precedes = makeCard.precedes;
 	card.sameColor = makeCard.sameColor;
-	//...
+	card.nextInSuit = makeCard.nextInSuit;
+	card.prevInSuit = makeCard.prevInSuit;
 	return card;
 }
 
@@ -63,7 +64,7 @@ makeCard.precedes = function(cardObj) {
         return(false);        
 	}
 makeCard.sameColor = function(cardObj) {
-        return(this.color(cardA)===this.color(cardB));
+        return(this.color()===cardObj.color());
 	}
 
 makeCard.nextInSuit = function() {
@@ -81,7 +82,6 @@ makeCard.prevInSuit = function() {
         return(this.id+48);
          }
 	}
-}
 
 var card34 = makeCard(34);
 var card13 = makeCard(13);
@@ -89,6 +89,52 @@ console.log(card34.rank());
 console.log(card34.suit());
 console.log(card34.cardID());
 console.log(card34.color());
-//console.log(card34.name()); //getting card.name a function error ?
+//console.log(card34.name()); //getting card.name is not a function error ?
 console.log(card13.precedes(card34));
+
+//1b) assertions
+
+var card0 = makeCard(0);
+var card3 = makeCard(3);
+var card51 = makeCard(51);
+var card1 = makeCard(1);
+var card2 = makeCard(2);
+var card4 = makeCard(4);
+var card13 = makeCard(13);
+var card3 = makeCard(3);
+var card30 = makeCard(30);
+var card5 = makeCard(5);
+var card51 = makeCard(51);
+var card4 = makeCard(4);
+var card50 = makeCard(50);
+var card48 = makeCard(48);
+
+function assert(claim,message) {
+    if (!claim) console.error(message);
+}
+assert(card0.rank()===1,"Test 1 failed");
+assert(card3.rank()===1,"Test 2 failed");
+assert(card51.rank()===13,"Test 3 failed");
+assert(card0.suit()===1,"Test 4 failed");
+assert(card5.suit()===2,"Test 5 failed");
+assert(card51.suit()===4,"Test 6 failed");
+assert(card0.cardID()===0,"Test 7 failed");
+assert(card51.cardID()===51,"Test 8 failed");
+assert(card30.cardID()===30,"Test 9 failed");
+assert(card0.color()==='red',"Test 10 failed");
+assert(card2.color()==='black',"Test 11 failed");
+//assert(alias.name(5)==='Two of Diamonds',"Test 12 failed");
+//assert(alias.name(51)==='King of Clubs',"Test 13 failed");
+assert(!card0.precedes(card1),"Test 14 failed");
+assert(card0.precedes(card5),"Test 15 failed");
+assert(card51.precedes(card0),"Test 16 failed");
+assert(card50.precedes(card2),"Test 17 failed");
+assert(card0.sameColor(card1),"Test 18 failed");
+assert(!card1.sameColor(card2),"Test 19 failed");
+assert(card0.nextInSuit()===4,"Test 20 failed");
+assert(card51.nextInSuit()===3,"Test 21 failed");
+assert(card48.nextInSuit()===0,"Test 22 failed");
+assert(card0.prevInSuit()===48,"Test 23 failed");
+assert(card3.prevInSuit()===51,"Test 24 failed");
+assert(card5.prevInSuit()===1,"Test 25 failed");
 
